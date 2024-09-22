576b80b3eab49720d6aa51934c5d1199b55dfd87
fullstakilla <selakovich@mail.ru>
2024-09-22 13:16:24 +0300

    done: admin auth + customer home page

---
+++ src/app/(customerFacing)/layout.tsx
@@ -0,0 +1,18 @@
+import { Nav, NavLink } from "@/components/Nav";
+
+export const dynamic = "force-dynamic"
+
+export default function Layout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return <>
+        <Nav>
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/products">Products</NavLink>
+          <NavLink href="/orders">Customers</NavLink>
+        </Nav>
+        <div className="container my-6 px-6">{children}</div>
+    </>
+}
